74c7949292aecb26bedcbe298623fa752c3de226
Seongjin Park <seongjinpark@email.arizona.edu>
2018-12-13 14:05:04 -0700

    first commit

---
+++ docker/build_container.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# script to begin the docker build process
+# NOTE: **MUST** be run from docker/
+
+# OPTIONAL ARGUMENTS
+# -w = for windows 10 Home
+# -m = set amount of memory docker can use during build (in G)
+
+win=false
+mem=4g
+
+while getopts "wm:" opt; do
+    case ${opt} in
+        w)
+            win=true
+            ;;
+        m)
+            mem=${OPTARG}
+            ;;
+        \?)
+            echo "Wrong flags"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ ${win} == true ]]; then
+    docker build \
+        -f WIN/Dockerfile \
+        -m ${mem} \
+        -t docker-timit ../
+else
+    docker build \
+        -f UNIX/Dockerfile \
+        -m ${mem} \
+        -t docker-timit ../
+fi
