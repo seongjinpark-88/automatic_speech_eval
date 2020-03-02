df2f2cf64a8ba894e85c83723e092182adca1205
Seongjin Park <seongjinpark@email.arizona.edu>
2020-03-02 15:09:20 -0700

    updating...

--- egs/timit/s5/run_clear_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script removes all files generated in `nextiva_recipes` after any step of the process.
-# Ensures a clean state
-
-rm -rf ${KALDI_INSTRUCTIONAL_PATH}/data/ \
-    ${KALDI_INSTRUCTIONAL_PATH}/mfcc/ \
-    ${KALDI_INSTRUCTIONAL_PATH}/exp/
-
