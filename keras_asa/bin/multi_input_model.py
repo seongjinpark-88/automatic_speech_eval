ed6dab128998105518298d8f756c36536873b2d8
john culnan <jmculnan@email.arizona.edu>
2020-03-10 16:35:52 -0700

    updating model architecture

--- keras_asa/bin/multi_input_model.py
+++ keras_asa/bin/multi_input_model.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pickle
 import random
 import pprint
+import warnings
 
 # set seed for reproducibility
 seed = 888
@@ -19,8 +20,9 @@ from keras import optimizers
 from keras.layers import Conv2D, MaxPooling2D
 from sklearn.metrics import classification_report
 
-#todo: finish data prep code, scaffold NN code in class AdaptiveModel
-#todo: ensure that we can actually get the feature sets we want -- work on this
+# todo: finish data prep code, test on phonetic features!
+# todo: ensure that we can actually get the feature sets we want -- work on this
+# todo: rename suprasegmental/segmental to phonological/phonetic, respectively
 
 
 class GetFeatures:
@@ -35,6 +37,9 @@ class GetFeatures:
         self.segment_name = None
 
     def get_features(self, output_name, supra=True):
+        """
+        Get the set of phonological/phonetic features
+        """
         for f in os.listdir(self.apath):
             if f.endswith('.wav'):
                 # todo: replace config files with the appropriate choice
@@ -75,6 +80,7 @@ class PrepareData:
         self.segmental = segmental_data
         self.suprasegmental = suprasegmental_data
         self.raw = raw_data
+        self.model_type = self.determine_model()
 
     def determine_model(self):
         """
@@ -101,9 +107,25 @@ class PrepareData:
     def concat_data(self):
         """
         Concatenate the segmental and suprasegmental data
+        todo: complete 'pass' statements for other model types
+        todo: determine how to concat raw data
         """
-        if self.segmental is not None and self.suprasegmental is not None:
+        if self.model_type == 'seg_and_supra':
             return np.concatenate((self.segmental, self.suprasegmental), axis=1)
+        elif self.model_type == 'seg_and_raw':
+            return self.segmental, self.raw
+        elif self.model_type == 'supra_and_raw':
+            return self.suprasegmental, self.raw
+        elif self.model_type == 'all':
+            return np.concatenate((self.segmental, self.suprasegmental), axis=1), self.raw
+        elif self.model_type == 'segmental':
+            return self.segmental
+        elif self.model_type == 'suprasegmental':
+            return self.suprasegmental
+        elif self.model_type == 'raw':
+            return self.raw
+        else:
+            return warnings.warn("Something is wrong here...model_type note found")
 
     def get_data_size(self):
         return self.concat_data().size()
@@ -114,6 +136,15 @@ class AdaptiveModel:
     Should allow for input of 1, 2, or all 3 types of data;
     todo: should all types be handled with the same architecture?
     Assumes that the y for each data point is the FINAL ELEMENT of the vector
+    Right now, this is agnostic to type of data--assumes concatenated elements
+    todo: we want to have models for the following
+            raw RNN, phonetic RNN, phonol MLP
+            raw + phonet RNN, raw + phonol ?, phonet + phonol ?
+            raw + phonet + phonol ?
+            for '?' we can do voting/weighted avg of scores
+               or we can do final layer of additional MLP
+            timestamped features better for RNN (phonetic LLDs)
+            could try this OR high level vector when combining
     """
     def __init__(self, data, data_size, outpath):
         self.data = data
@@ -167,11 +198,38 @@ class AdaptiveModel:
         testy = pickle.load("{0}/{1}".format(self.save_path, test_y_file))
         return trainX, trainy, valX, valy, testX, testy
 
-    def define_model(self, n_lstm=2, n_lstm_units=50, dropout=0.2, n_connected=1,
-                     n_connected_units=25, l_rate = 0.001, beta_1=0.9, beta_2=0.999,
-                     act='relu', output_act='softmax',loss_fx='mean_squared_error'):
+    def mlp_model(self, n_connected=2, n_connected_units=25, l_rate=0.001,
+                  dropout=0.2, beta_1=0.9, beta_2=0.999, act='relu',
+                  output_act='softmax', loss_fx='mean_squared_error',
+                  output_size=7):
         """
-        Initialize the model
+        Initialize the MLP model
+        n_connected:            the number of mlp layers
+        n_connected_units:      number of cells in mlp layers
+        dropout:                dropout rate in mlp layers
+        beta_1:                 value of beta 1 for Adam
+        beta_2:                 value of beta 2 for Adam
+        act:                    the activation function in lstm + dense layers
+        output_act:             the activation function in the final layer
+        output_size:            the length of predictions vector; default is 7
+        """
+        while n_connected > 0:
+            self.model.add(Dense(n_connected_units, input_dim=self.data_size[1],
+                                 activation=act, dropout=dropout))
+            n_connected -= 1
+        # add the final layer with output activation
+        self.model.add(Dense(output_size, activation=output_act))
+        # set an optimizer -- adam with default param values
+        opt = optimizers.Adam(learning_rate=l_rate, beta_1=beta_1, beta_2=beta_2)
+        # compile the model
+        self.model.compile(loss=loss_fx, optimizer=opt, metrics=['acc'])
+
+    def lstm_model(self, n_lstm=2, n_lstm_units=50, dropout=0.2, n_connected=1,
+                     n_connected_units=25, l_rate = 0.001, beta_1=0.9, beta_2=0.999,
+                     act='relu', output_act='softmax', loss_fx='mean_squared_error',
+                     output_size=7):
+        """
+        Initialize the LSTM-based model
         n_lstm:                 number of lstm layers
         n_lstm_units:           number of lstm cells in each layer
         dropout:                dropout rate in lstm layers
@@ -181,11 +239,12 @@ class AdaptiveModel:
         beta_2:                 value of beta 2 for Adam
         act:                    the activation function in lstm + dense layers
         output_act:             the activation function in the final layer
+        output_size:            the length of predictions vector; default is 7
         """
         # add all the hidden layers
         while n_lstm > 0:
-            self.model.add(LSTM(n_lstm_units, input_dim=self.data_size[1],
-                                activation=act, dropout=dropout))
+            self.model.add(Bidirectional(LSTM(n_lstm_units, input_dim=self.data_size[1],
+                                              activation=act, dropout=dropout)))
             n_lstm -= 1
         # add the connected layers
         while n_connected > 0:
@@ -193,16 +252,28 @@ class AdaptiveModel:
                                  activation=act))
             n_connected -= 1
         # add the final layer with output activation
-        self.model.add(Dense(7, activation=output_act))
+        self.model.add(Dense(output_size, activation=output_act))
         # set an optimiser -- adam with default param values
         opt = optimizers.Adam(learning_rate=l_rate, beta_1=beta_1, beta_2=beta_2)
         # compile the model
         self.model.compile(loss=loss_fx, optimizer=opt, metrics=['acc'])
 
+    def final_layers(self, n_connected=1, n_connected_units=25, l_rate=0.001,
+                  dropout=0.2, beta_1=0.9, beta_2=0.999, act='relu',
+                  output_act='softmax', loss_fx='mean_squared_error'):
+        """
+        Initialize the MLP model as that takes output from lstm + mlp models and combines
+        """
+        # create an mlp layer
+        # create an lstm layer
+        # compile mlp and lstm
+        # train the final layer model on the TRAINING output of each model
+
     def train_and_predict(self, trainX, trainy, valX, valy, batch=32, num_epochs=100):
         """
         Train the model
-        batch: minibatch size
+        batch:              minibatch size
+        num_epochs:         number of epochs
         """
         # fit the model to the data
         self.model.fit(trainX, trainy, batch_size=batch, epochs=num_epochs, shuffle=True)
